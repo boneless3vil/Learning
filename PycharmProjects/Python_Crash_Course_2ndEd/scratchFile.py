3b96f899810e6c304f8e121735108aeff67bf594
Jonathan  Baldwin <jon@sinoverpi.com>
2022-12-03 18:15:35 -0800

    I have no idea if I'm doing this right

--- PycharmProjects/Python_Crash_Course_2ndEd/scratchFile.py
+++ PycharmProjects/Python_Crash_Course_2ndEd/scratchFile.py
@@ -1,16 +1,27 @@
+name_prompt = "\nWhat's your name? "
+place_prompt = "If you could visit one place in the world, where would it be? "
+continue_prompt = "\nWould you like to let someone else respond? (yes/no) "
 
-beginning_list = ['item 1', 'item 2', 'item 3']
-ending_list = []
+# Responses will be stored in the form {name: place}.
+responses = {}
 
-while beginning_list:
-    item_ending_list = beginning_list.pop() #each item popped out of beginning_list --> item_ending_list
-    print(f"Item in ending_list: {item_ending_list}")   #print each item as it enters item_ending_list
-    ending_list.append(item_ending_list)    # now add each item in that list to ending_list
+while True:
+    # Ask the user where they'd like to go.
+    name = input(name_prompt)
+    place = input(place_prompt)
 
-print(f"\nEnding_list items:")
-for e_u in ending_list: # grab each item in the list, one by one
-    print(e_u.title())  # print each item
+    # Store the response.
+    responses[name] = place
 
+    # Ask if there's anyone else responding.
+    repeat = input(continue_prompt)
+    if repeat != 'yes':
+        break
+
+# Show results of the survey.
+print("\n--- Results ---")
+for name, place in responses.items():
+    print(f"{name.title()} would like to visit {place.title()}.")
 
 
 
